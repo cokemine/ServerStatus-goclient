cokemine/ServerStatus-goclient

commit 6cb452bd556305c04e4736f34e13855b0d1d9c4d

fix: fix wrong uptime and swap memory stats for LXC VPS
fix https://github.com/cokemine/nodestatus-client-go/issues/2

pkg/status/status.go
@@ -31,14 +31,13 @@ func NewNetwork() *network {
 }
 
 func Uptime() uint64 {
-	uptime, _ := host.Uptime()
-	return uptime
+	bootTime, _ := host.BootTime()
+	return uint64(time.Now().Unix()) - bootTime
 }
 
 func Memory() (uint64, uint64, uint64, uint64) {
 	memory, _ := mem.VirtualMemory()
-	swap, _ := mem.SwapMemory()
-	return memory.Total / 1024.0, memory.Used / 1024.0, swap.Total / 1024.0, swap.Used / 1024.0
+	return memory.Total / 1024.0, memory.Used / 1024.0, memory.SwapTotal / 1024.0, (memory.SwapTotal - memory.SwapFree) / 1024.0
 }
 
 func Load() float64 {

commit 33c226427d92f082165116213ea4a2f73e58220a

fix: fix no swap info on darwin and windows
probably caused by https://github.com/cokemine/ServerStatus-goclient/commit/6cb452bd556305c04e4736f34e13855b0d1d9c4d

See: https://github.com/cokemine/nodestatus-client-go/issues/18#issuecomment-1104040441

pkg/status/mem.go
@@ -1,0 +1,12 @@
+//go:build !linux
+// +build !linux
+
+package status
+
+import "github.com/shirou/gopsutil/v3/mem"
+
+func Memory() (uint64, uint64, uint64, uint64) {
+	memory, _ := mem.VirtualMemory()
+	swap, _ := mem.SwapMemory()
+	return memory.Total / 1024.0, memory.Used / 1024.0, swap.Total / 1024.0, swap.Used / 1024.0
+}

pkg/status/mem_linux.go
@@ -1,0 +1,11 @@
+//go:build linux
+// +build linux
+
+package status
+
+import "github.com/shirou/gopsutil/v3/mem"
+
+func Memory() (uint64, uint64, uint64, uint64) {
+	memory, _ := mem.VirtualMemory()
+	return memory.Total / 1024.0, memory.Used / 1024.0, memory.SwapTotal / 1024.0, (memory.SwapTotal - memory.SwapFree) / 1024.0
+}

pkg/status/status.go
@@ -5,7 +5,6 @@ import (
 	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
 	"github.com/shirou/gopsutil/v3/load"
-	"github.com/shirou/gopsutil/v3/mem"
 	psutilNet "github.com/shirou/gopsutil/v3/net"
 	"math"
 	"net"
@@ -34,11 +33,6 @@ func NewNetwork() *network {
 func Uptime() uint64 {
 	bootTime, _ := host.BootTime()
 	return uint64(time.Now().Unix()) - bootTime
-}
-
-func Memory() (uint64, uint64, uint64, uint64) {
-	memory, _ := mem.VirtualMemory()
-	return memory.Total / 1024.0, memory.Used / 1024.0, memory.SwapTotal / 1024.0, (memory.SwapTotal - memory.SwapFree) / 1024.0
 }
 
 func Load() float64 {